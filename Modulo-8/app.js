df051be59ff09de8fb45defd958647880da53aba
Cesar Augusto <cesinhalius@hotmail.com>
2025-01-06 11:21:28 -0300

    Adicionando arquivos de como consumir APIs de terceiros

---
+++ Modulo-8/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/:cep", async (req, res) => {
+	const cep = req.params.cep;
+	try {
+		const response = await fetch(`https://viacep.com.br/ws/${cep}/json`);
+		const data = await response.json();
+		if (data.erro) {
+			res.status(400).json({ mensagem: "Um erro foi encontrado." });
+		} else {
+			res.json({
+				logradouro: data.logradouro,
+				bairro: data.bairro,
+				localidade: data.localidade,
+				uf: data.uf,
+				cep: data.cep,
+				ibge: data.ibge,
+			});
+		}
+	} catch (erro) {
+		res.status(500).json({ mensagem: "Erro interno no servidor" });
+	}
+});
+
+app.listen(8000);
